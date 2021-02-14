b46a2a85d5fa667ff19844f42d038900ac8f9e85
Pranav Kutty <pranavkutty13@gmail.com>
2021-02-14 20:18:18 +0530

    edit server scripts and frontend

--- test_server.sh
+++ test_server.sh
@@ -5,7 +5,6 @@
 
 # cd to the cloned repo directory
 
-cd pranavkutty13-me_buildout_xmeme/
 
 # Run the user’s installation steps which will install any necessary dependencies required for the server to run, with sudo permission
 
